78ab8b51b96a0914075abc6b8d2a7434931c0cd8
“joaopaulosilvasimoes” <joaopaulosilvasimoes@gmail.com>
2022-07-20 15:56:03 -0300

    Public class

---
+++ chapter_1/Animal.java
@@ -0,0 +1,15 @@
+package chapter_1;
+
+// If you do have a public type, it needs to match the filename.
+public class Animal {
+
+    public static void main(String[] args){
+
+    }
+    
+}
+
+//public class Animal2{
+class Animal2 {
+
+}
