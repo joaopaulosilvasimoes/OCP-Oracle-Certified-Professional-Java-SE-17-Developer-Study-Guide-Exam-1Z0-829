c82ef06705647c55bbe69274e388e20d2303b482
“joaopaulosilvasimoes” <joaopaulosilvasimoes@gmail.com>
2022-07-19 22:53:19 -0300

    Commit Inicial

---
+++ chapter_1/Comments.java
@@ -0,0 +1,25 @@
+package chapter_1;
+
+/* 
+ * // anteater 
+ */ 
+
+// bear 
+
+// // cat 
+
+// /* dog */ 
+
+/* elephant */ 
+
+/*
+ * /* ferret */
+ */
+
+public class Comments{
+
+    public static void main(String[] args) {
+    
+    }
+
+}
