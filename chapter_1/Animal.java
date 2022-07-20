71ed764788c381c7349dfde71779cdc10b2e0706
“joaopaulosilvasimoes” <joaopaulosilvasimoes@gmail.com>
2022-07-20 17:28:13 -0300

    Main method

--- chapter_1/Animal.java
+++ chapter_1/Animal.java
@@ -1,6 +1,7 @@
 package chapter_1;
 
 // If you do have a public type, it needs to match the filename.
+// When you do so, at most one of the top- level types in the file is allowed to be public.
 public class Animal {
 
     public static void main(String[] args){
---
+++ chapter_1/Zoo.java
@@ -0,0 +1,21 @@
+//javac
+//java chapter_1/Zoo Bronx Zoo
+//java chapter_1/Zoo "San Diego" Zoo
+
+package chapter_1;
+
+public class Zoo {
+
+    // Both final modifiers are optional, and the main() method is a valid entry point with or without them. 
+    public final static void main(final String[] args){
+    //public static void main(String[] args){    
+
+        System.out.println("Hello World");
+        
+        System.out.println(args[0]);
+
+        System.out.println(args[1]);
+
+    }
+    
+}
